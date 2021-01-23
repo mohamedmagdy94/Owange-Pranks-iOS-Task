ce3c96e811dee9de3cef649b205c22c94214ad9a
Mohamed ElTaweel <mohamedmagdy.9080@gmail.com>
2021-01-23 20:55:42 +0200

    Initial Commit

---
+++ Owange-Pranks-iOS-TaskTests/Owange_Pranks_iOS_TaskTests.swift
@@ -0,0 +1,34 @@
+//
+//  Owange_Pranks_iOS_TaskTests.swift
+//  Owange-Pranks-iOS-TaskTests
+//
+//  Created by Mohamed El-Taweel on 1/23/21.
+//  Copyright © 2021 OwangePranks. All rights reserved.
+//
+
+import XCTest
+@testable import Owange_Pranks_iOS_Task
+
+class Owange_Pranks_iOS_TaskTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
